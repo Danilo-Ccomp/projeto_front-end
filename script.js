e5a98936a55b861a399d4185bb6303b95d050ec3
Danilo Silva <danilos282519@ufrrj.br>
2025-01-26 18:29:17 -0300

    relative paths

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function toggleMode() {
     // se tiver light mode
     // substituir a img
     if (html.classList.contains('light')) {
-        img.setAttribute('src', '/Assets/perfil_oculos.jpeg')
+        img.setAttribute('src', './Assets/perfil_oculos.jpeg')
         img.setAttribute('alt', 'Foto de perfil Danilo Silva no light mode')
         //senão, manter img
     } else {
-        img.setAttribute('src', '/Assets/perfil.jpeg')
+        img.setAttribute('src', './Assets/perfil.jpeg')
         img.setAttribute('alt', 'Foto de perfil Danilo Silva')
     }
 }
